cd12dd4bcd585a9d73ea0a836ee17667997bf62f
Ari <a.craciun@student.tue.nl>
2023-11-23 17:59:10 +0100

    Ari clean

--- Clean.py
+++ Clean.py
@@ -35,3 +35,30 @@ for i in range(len(df['Payment_Behaviour'])):
 for i in range(len(df['Credit_History_Age'])):
     df['Credit_History_Age'][i] = KeepAlphabetsNumbers(df['Credit_History_Age'][i])
     # Remove everything except for A-Z and Numbers
+
+
+
+df['Annual_Income'] = df['Annual_Income'].replace('[^\d.,]', '', regex=True).str.replace(',', '.', regex=False).astype(float)
+df['Monthly_Inhand_Salary'] = df['Monthly_Inhand_Salary'].replace('[^\d.,]', '', regex=True).str.replace(',', '.', regex=False).astype(float)
+df['Outstanding_Debt'] = df['Outstanding_Debt'].replace('[^\d.,]', '', regex=True).str.replace(',', '.', regex=False).astype(float)
+df['Total_EMI_per_month'] = df['Total_EMI_per_month'].replace('[^\d.,]', '', regex=True).str.replace(',', '.', regex=False).astype(float)
+df['Amount_invested_monthly'] = df['Amount_invested_monthly'].replace('[^\d.,]', '', regex=True).str.replace(',', '.', regex=False).astype(float)
+df['Monthly_Balance'] = df['Monthly_Balance'].replace('[^\d,]', '', regex=True).str.replace(',', '.', regex=False).astype(float)
+df['Monthly_Balance'] = pd.to_numeric(df['Monthly_Balance'], errors='coerce')
+df['Num_Credit_Card'] = df['Num_Credit_Card'].replace('[^\d,]', '', regex=True)
+df['Num_Credit_Card'] = pd.to_numeric(df['Num_Credit_Card'], errors='coerce', downcast='integer') #this one still has issues
+df['Interest_Rate'] = df['Interest_Rate'].replace('[^\d,]', '', regex=True)
+df['Interest_Rate'] = pd.to_numeric(df['Interest_Rate'], errors='coerce', downcast='integer')
+df['Num_of_Loan'] = df['Num_of_Loan'].replace('[^\d,]', '', regex=True)
+df['Num_of_Loan'] = pd.to_numeric(df['Num_of_Loan'], errors='coerce', downcast='integer')
+df['Num_of_Delayed_Payment'] = df['Num_of_Delayed_Payment'].replace('[^\d,]', '', regex=True)
+df['Num_of_Delayed_Payment'] = pd.to_numeric(df['Num_of_Delayed_Payment'], errors='coerce', downcast='integer')
+df['Num_Credit_Inquiries'] = df['Num_Credit_Inquiries'].replace('[^\d,]', '', regex=True)
+df['Num_Credit_Inquiries'] = pd.to_numeric(df['Num_Credit_Inquiries'], errors='coerce', downcast='integer')
+df['Credit_Utilization_Ratio'] = df['Credit_Utilization_Ratio'].replace('[^\d.,]', '', regex=True).str.replace(',', '.', regex=False).astype(float)
+# deal with Changed_Credit_Limit
+# deal with Credit_History_Age
+# map Payment_of_Min_Amount
+# map Total_EMI_per_month
+# one hot encode Payment_Behaviour
+# map Credit_Score
