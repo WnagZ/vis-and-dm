9508684f3f641360494f2bb66b6764dc3dd5bdf4
Wang <131781679+WnagZ@users.noreply.github.com>
2023-11-30 19:16:32 +0100

    added multiple argmin

--- coordinatedescent.py
+++ coordinatedescent.py
@@ -1,23 +1,62 @@
 import numpy as np
 from sympy import symbols, diff
 
-def coordinate_descent(f, argmin, x_0, max_iter=100):
-    # y = symbols('y')
-    for i in range(max_iter):
-        for counter in range(3):
-            # x_0[counter] = y
-            x_0[counter] = argmin_xi(f)
-    return x_0
+import numpy as np
 
-def argmin_xi(x):
-    #diff(f, y)
-    return np.argmin(x)
+
+def f(x):
+    return 0.5 * x[0] ** 4 - x[0] * x[1] + x[1] ** 2 + x[1] * x[2] + x[2] ** 2
+
+def argmin_x1(x):
+    return np.argmin(1.5*x[1]**3 - 1)
+
+    # return (x[1] ** 2 + x[1] * x[2]) / (2 * x[0] ** 3 - 1)
+
+
+def argmin_x2(x):
+    return np.argmin(- x[0] + 2*x[1] + x[2])
+    # return (x[0] ** 2 + x[0] * x[2] - x[0]) / (2 * x[2])
+
+
+def argmin_x3(x):
+    return np.argmin(x[1] + 2*x[2])
+    # return (x[0] * x[1] + 2 * x[2]) / (x[0] ** 2 + x[1] ** 2)
+
+
+def coordinate_descent(argmin, x_0, max_iter=100):
+    # y = symbols('y')
+    x_t = x_0.copy()
+    for i in range(max_iter):
+        for j in range(len(x_t)):
+            x_t[j] = argmin[j](x_t)
+    return x_t
 
 
 # Perform coordinate descent.
 x_0 = [1, 1, 1]
-f = 0.5 * x_0[0] ** 4 - x_0[0] * x_0[1] + x_0[1] ** 2 + x_0[1] * x_0[2] + x_0[2] ** 2
-coordinate_descent(f, argmin_xi, x_0)
+argmin = [argmin_x1, argmin_x2, argmin_x3]
+x_min = coordinate_descent(argmin, x_0)
 
 # Print the second coordinate of the minimizer.
-print(x_0[1])
+print(x_min[1])
+
+# def coordinate_descent(f, argmin, x_0, max_iter=100):
+#     # y = symbols('y')
+#     for i in range(max_iter):
+#         for counter in range(3):
+#             # x_0[counter] = y
+#             x_0[counter] = argmin_xi(f)
+#     return x_0
+#
+# def argmin_xi(x):
+#     #diff(f, y)
+#     return np.argmin(x)
+#
+#
+# # Perform coordinate descent.
+# x_0 = [1, 1, 1]
+# f = 0.5 * x_0[0] ** 4 - x_0[0] * x_0[1] + x_0[1] ** 2 + x_0[1] * x_0[2] + x_0[2] ** 2
+# coordinate_descent(f, argmin_xi, x_0)
+#
+# # Print the second coordinate of the minimizer.
+# print(x_0[1])
